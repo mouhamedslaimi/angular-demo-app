cac9cde0c191553203a44d02d2b6fa0e423554c7
mouhamedslaimi <mouhamedslaimi@gmail.com>
2023-09-29 16:07:41 +0100

    Add observables example

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
   styleUrls: ['./observables.component.css']
 })
-export class ObservablesComponent {
-
+export class ObservablesComponent implements OnInit {
+  data : number[] = [];
+  count = 0;
+  myObservables : any;
+  constructor(){}
+  ngOnInit(): void {
+    this.myObservables = new Observable<number[]>((observer)=>{
+      setInterval(()=>{
+        this.count++;
+        this.data.push(this.count);
+        if(this.count < 5){
+          observer.next(this.data);
+        }else{
+         // observer.complete();
+         observer.error("DB connection is time out");
+        }
+      },1000)
+    })
+  }
+  getData(){
+    this.myObservables.subscribe((res : any)=>{
+      console.log(res);
+    }, (err :any)=>{
+      console.log(err);
+    }, ()=>{
+      console.log('Completed');
+    })
+    console.log(this.data);
+  }
 }
